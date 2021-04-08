f74c71158f3a0907fa7164c7a77d8d85bc5f5162
Bruno Rodrigues <bytebruno@gmail.com>
2021-04-08 12:07:34 -0300

    feat: Added ngrx and angular-material dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
 
-import { AppComponent } from './app.component'
+import { AppComponent } from './app.component';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, StoreModule.forRoot({}, {}), EffectsModule.forRoot([]), BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
