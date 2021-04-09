d5d1ca3ce89b4e925591332790b656e45bdbd39c
Bruno Rodrigues <bytebruno@gmail.com>
2021-04-09 13:01:13 -0300

    feat: Core module and additional settings
    - Created CoreModule to organize what should be eagerly loaded in the app (basic layout and singleton core services)
    - Updated angular schematics to generate component with OnPush change detection by default.
    - Changed the prefix to internal components to "bb" (backbase)
    - Added webpack-bundle-analyzer to understand the content of generated bundles (size, structure, etc)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,11 @@
+import { AppComponent } from './app.component'
+import { AppRoutingModule } from './app-routing.module'
+import { CoreModule } from './core/core.module'
 import { NgModule } from '@angular/core'
-import { BrowserModule } from '@angular/platform-browser'
-
-import { AppComponent } from './app.component';
-import { StoreModule } from '@ngrx/store';
-import { EffectsModule } from '@ngrx/effects';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, StoreModule.forRoot({}, {}), EffectsModule.forRoot([]), BrowserAnimationsModule],
+  imports: [CoreModule],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ src/app/core/core.module.ts
@@ -0,0 +1,27 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { BrowserModule } from '@angular/platform-browser'
+import { EffectsModule } from '@ngrx/effects'
+import { MainLayoutComponent } from './layout/main-layout/main-layout.component'
+import { MatToolbarModule } from '@angular/material/toolbar'
+import { NgModule } from '@angular/core'
+import { RouterModule } from '@angular/router'
+import { StoreModule } from '@ngrx/store'
+
+@NgModule({
+  declarations: [MainLayoutComponent],
+  imports: [
+    // vendor
+    BrowserModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot([]),
+
+    // ngrx
+    EffectsModule.forRoot([]),
+    StoreModule.forRoot({}, {}),
+
+    // material
+    MatToolbarModule,
+  ],
+  exports: [MainLayoutComponent],
+})
+export class CoreModule {}
