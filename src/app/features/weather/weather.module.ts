457384f2f9636215b58870b89953d33b476fd2e3
Bruno Rodrigues <bytebruno@gmail.com>
2021-04-09 19:53:12 -0300

    feat: city-card component template created
    - Added weathericons to the project
    - Edited city-card component

--- src/app/features/weather/weather.module.ts
+++ src/app/features/weather/weather.module.ts
@@ -1,23 +1,26 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
-import { WeatherRoutingModule } from './weather-routing.module';
-import { WeatherComponent } from './weather.component';
-import { CitiesListComponent } from './components/cities-list/cities-list.component';
-import { CityCardComponent } from './components/city-card/city-card.component';
-import { CityDetailsComponent } from './components/city-details/city-details.component';
-
+import { CitiesListComponent } from './components/cities-list/cities-list.component'
+import { CityCardComponent } from './components/city-card/city-card.component'
+import { CityDetailsComponent } from './components/city-details/city-details.component'
+import { CommonModule } from '@angular/common'
+import { MatCardModule } from '@angular/material/card'
+import { NgModule } from '@angular/core'
+import { WeatherComponent } from './weather.component'
+import { WeatherRoutingModule } from './weather-routing.module'
 
 @NgModule({
   declarations: [
     WeatherComponent,
     CitiesListComponent,
     CityCardComponent,
-    CityDetailsComponent
+    CityDetailsComponent,
   ],
   imports: [
+    // vendor
     CommonModule,
-    WeatherRoutingModule
-  ]
+    WeatherRoutingModule,
+
+    // material
+    MatCardModule,
+  ],
 })
-export class WeatherModule { }
+export class WeatherModule {}
